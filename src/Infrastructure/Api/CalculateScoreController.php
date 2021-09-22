73fe5d73f88c7ced65bdb84f8d5b9e929f556c86
Gabi Piñeiro <gabispineiro@gmail.com>
2021-09-22 18:59:37 +0200

    created route scores

--- src/Infrastructure/Api/CalculateScoreController.php
+++ src/Infrastructure/Api/CalculateScoreController.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Infrastructure\Api;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class CalculateScoreController
 {
+    #[Route('/scores', name:'scores')]
     public function __invoke(): JsonResponse
     {
         return new JsonResponse([]);
